d802e6d5738d2155e0150341ff8cd73eddb93f28
Flávio Reis <flavioareis1@gmail.com>
2024-09-16 19:12:10 -0300

    att processText.js

--- processText.js
+++ processText.js
@@ -1,11 +1,3 @@
-import { pipeline } from '@xenova/transformers';
-
-async function processText(inputText) {
-    const model = await pipeline('text2text-generation', 't5-small'); // Ou qualquer outro modelo adequado
-    const output = await model(inputText);
-    return output[0].generated_text;
-}
-
 const inputText = process.argv[2];
 
 processText(inputText).then(outputText => {
